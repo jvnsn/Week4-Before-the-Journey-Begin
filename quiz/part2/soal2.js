82b82b2496ea77400bf9e739057dfa427f1ac630
jvnsn <joviansenwidyanto@google.com>
2025-05-11 15:59:57 +0800

    part 2

---
+++ quiz/part2/soal2.js
@@ -0,0 +1,38 @@
+/*
+Diberikan function meleeRangedGrouping yang menerima 1 parameter berupa string, implementasikan meleeRangedGrouping agar dapat menghasilkan multidimensional array seperti yang diminta.
+
+Format string yang diberikan adalah: <nama_hero>-<tipe_hero>,<nama_hero>-<tipe-hero>, ...
+
+Output yang diharapkan: [ [ <daftar_hero_dengan_tipe_ranged> ], [ <daftar_hero_dengan_tipe_melee> ] ]
+
+Jika input adalah string kosong ('') maka return array kosong
+*/
+
+function meleeRangedGrouping (str) {
+    //your code here
+    if (!str) return [];
+    let hero = str.split(',');
+    let ranged = [];
+    let melee = [];
+
+    for(let i = 0; i < hero.length; i++) {
+        let type = hero[i].split('-');
+        if (type[1] === 'Ranged') {
+            ranged.push(type[0]);
+        } else {
+            melee.push(type[0]);
+        }
+    }
+
+    return [ranged, melee];
+  }
+  
+  // TEST CASE
+  
+  console.log(meleeRangedGrouping('Razor-Ranged,Invoker-Ranged,Meepo-Melee,Axe-Melee,Sniper-Ranged'));
+  // [ ['Razor', 'Invoker', 'Sniper'], ['Meepo', 'Axe'] ]
+  
+  console.log(meleeRangedGrouping('Drow Ranger-Ranged,Chen-Ranged,Dazzle-Ranged,Io-Ranged'));
+  // [ ['Drow Ranger', 'Chen', 'Dazzle', 'Io'], [] ]
+  
+  console.log(meleeRangedGrouping('')); // []
